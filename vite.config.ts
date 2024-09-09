af516a8ac272b00169c26a4d12fed13cf7da4431
kanakanho <76000830+kanakanho@users.noreply.github.com>
2024-09-09 12:37:37 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pagesのリポジトリ名をここに設定します
-const repoName = "sensor-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  base: process.env.GITHUB_PAGES ? "sensor-react" : "./"
 });
